64edac24950a0b627c61b820b29afdaa458ac2c3
Simeon <gangosimeon@gmail.com>
2025-09-25 20:03:05 +0000

    .

--- src/app/pages/dashboards/client-dashboard/client-dashboard.component.ts
+++ src/app/pages/dashboards/client-dashboard/client-dashboard.component.ts
@@ -2094,7 +2094,7 @@ export class ClientDashboardComponent implements OnInit {
       this.notificationService.showError("Erreur", "Agence non trouvée");
       return;
     }
-    this.messageData.sender = this.currentUser?._id || "";
+    this.messageData.sender = this.currentUser?.userId || "";
     this.messageData.receiver = this.receivedId || "";
     this.messageData.content = this.messageData.content.trim();
     if (!this.messageData.content) {
