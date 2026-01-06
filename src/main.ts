d16a57000e16c668ecbae01ee6c69757fe880f66
Simeon <gangosimeon@gmail.com>
2026-01-06 15:57:38 +0000

    REMPLACER

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
@@ -11,16 +11,11 @@ import { MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
-// Import des modules Material utilisés globalement
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideAnimations(),
     provideZoneChangeDetection(),
     provideRouter(routes),
     provideHttpClient(
@@ -33,13 +28,7 @@ bootstrapApplication(AppComponent, {
       }
     }),
     importProvidersFrom(
-      BrowserAnimationsModule,
-      MatIconModule,
-      MatButtonModule,
-      MatInputModule,
-      MatFormFieldModule,
-      MatCardModule,
-      MatProgressSpinnerModule
+      BrowserAnimationsModule
     ),
     MessageService
   ]
