808ea1f1b8435eec3cabef3cbaf965994822d25f
Simeon <gangosimeon@gmail.com>
2026-01-28 17:42:06 +0000

    client dashboard update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
 
   //URL en pre prod
-  apiUrl:'https://waste-6k43.onrender.com/api',
+  apiUrl:'http://213.32.120.11:3000/api',
   walletUrl:'https://waste-6k43.onrender.com/wallet'
 };
