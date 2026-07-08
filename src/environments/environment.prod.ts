902da7bd65de82bcd66a9eb1f58494bba4190a1b
W.Paulin GUIGMA <guigmawpaulin@gmail.com>
2026-07-08 13:08:07 +0200

    env on sertia

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://api.solidar.sertia.kerub.fr/solidar/api'
-  apiUrl:'https://waste-6k43.onrender.com/api',
+  apiUrl: 'https://api.solidar.sertia.kerub.fr/solidar/api'
+  // apiUrl:'https://waste-6k43.onrender.com/api',
 };
